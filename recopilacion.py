ce0d8b71fe88edc16e8917c0156f7cf5b983d207
Fer <fprogrammeur@gmail.com>
2025-05-29 18:46:49 +0200

    cliente, apellido, nombre, edad e ingreso 6 meses

--- recopilacion.py
+++ recopilacion.py
@@ -38,10 +38,26 @@ for i in range(num_clientes):
             break
         print("El correo electrónico no puede estar vacío y debe contener solo una '@'.")
 
+    # Registro de ingresos mensuales
+    meses = ["Mes 1", "Mes 2", "Mes 3", "Mes 4", "Mes 5", "Mes 6"]
+    total_ingresos = 0
+
+    for mes in meses:
+        while True:
+            try:
+                ingreso = float(input(f"Ingrese el ingreso para {mes}: "))
+                if ingreso >= 0:
+                    total_ingresos += ingreso
+                    break
+                print("No es posible que sea negativo, ingréselo de nuevo.")
+            except ValueError:
+                print("Entrada no válida. Debe ser un número.")
+
     # Crear la tarjeta de presentación para cada cliente
     print("\n===== Tarjeta de Presentación =====")
     print(f"Nombre: {nombre}")
     print(f"Apellido: {apellido}")
     print(f"Edad: {edad} ({clasificacion})")
     print(f"Correo Electrónico: {mail}")
+    print(f"Presupuesto total últimos 6 meses: ${total_ingresos:.2f}")
     print("===================================")
